550f26aa126ddfb5619301c2e8cb9c87bd728800
Konst9 <konst.mih.ter@gmail.com>
2024-08-20 13:43:47 +0300

    Linked product
    - add reducers for linked product
    - add hook useLoadLinkedProducts
    - add component LinkedProducts

--- src/view/components/product-page/index.tsx
+++ src/view/components/product-page/index.tsx
@@ -1,22 +1,26 @@
-import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
+import React, {FC} from 'react';
+import {useSelector} from 'react-redux';
 import { RootState } from '../../../store';
 import { ProductCard } from '../Card/ProductCard';
 import {useLoadProduct} from "../../hooks/useLoadProducts";
 import {useParams} from "react-router-dom";
+import {useLoadLinkedProducts} from "../../hooks/useLoadLinkedProducts";
+import {LinkedProducts} from "../Card/LinkedProducts";
 
 export const ProductPage: FC = () => {
   const { productId } = useParams<{ productId: string }>();
   const product = useSelector((state: RootState) => state.productPage.product || null);
+  const linkedProducts = useSelector((state: RootState) => state.productPage.linkedProducts);
 
   useLoadProduct(productId);
+  useLoadLinkedProducts(productId);
+
 
 
   return (
     <div className="product-page">
-      <div className="product-card">
-        <ProductCard product={product} />
-      </div>
+      <ProductCard product={product} />
+      <LinkedProducts linkedProducts={linkedProducts} />
     </div>
   );
 };
